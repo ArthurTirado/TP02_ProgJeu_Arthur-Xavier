8db7344f2a25844c592f815da60fffcb7ebce2c6
Arthur Tirado <arthurmegiolaro@gmail.com>
2024-05-02 11:45:15 -0400

    Game TitleScreen Added

--- ETU/Game/TitleScene.h
+++ ETU/Game/TitleScene.h
@@ -1,10 +1,19 @@
 #pragma once
 #include "Scene.h"
 #include "ContentManager.h"
+#include "TitleScreenContentManager.h"
+
 class TitleScene :
     public Scene
 {
 public:
+   static const std::string GAME_NAME;
+   static const std::string PROMPT;
+   static const int TITLE_TEXT_SIZE;
+   static const int SMALL_TEXT_SIZE;
+
+
+
   // Héritées via Scene
   TitleScene();
   ~TitleScene();
@@ -16,5 +25,10 @@ public:
 private:
   sf::Texture menuImageTexture;
   sf::Sprite menuImage;
+  sf::Text gameTitle;
+  sf::Text prompt;
+  sf::Music titleScreenMusic;
+  sf::RectangleShape blackBar;
+  TitleScreenContentManager contentManager;
 };
 
