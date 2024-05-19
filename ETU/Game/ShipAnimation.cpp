94ce92af599a9ca0d846cd329e8113d7e76f8c25
Xavbl <2237907@csfoy.ca>
2024-05-19 01:33:27 -0400

    Animations

---
+++ ETU/Game/ShipAnimation.cpp
@@ -0,0 +1,65 @@
+#include "stdafx.h"
+#include "ShipAnimation.h"
+#include "Inputs.h"
+
+const int ShipAnimation::NEUTRAL_ANIMATION_LENGTH_IN_SECONDS = 1.5;
+const int ShipAnimation::NBS_NEUTRAL_ANIMATION_FRAMES = 4;
+
+ShipAnimation::ShipAnimation(sf::Sprite& sprite)
+	: InputBasedAnimation(sprite), LinearAnimation(sprite, NEUTRAL_ANIMATION_LENGTH_IN_SECONDS, true)
+{
+}
+
+void ShipAnimation::adjustNextFrame(const Inputs& inputs)
+{
+	if (inputs.moveFactorX < 0 && nbFrameInStateX <= 50) {
+		nbFrameInStateX++;
+	}
+	else if (inputs.moveFactorX > 0 && nbFrameInStateX >= 0) {
+		nbFrameInStateX--;
+	}
+
+	if (nbFrameInStateX > 25) {
+		nbFrameInStateX--;
+	}
+	else if (nbFrameInStateX < 25) {
+		nbFrameInStateX++;
+	}
+
+	if (nbFrameInStateX > 40) {
+		nextFrame = 4;
+	}
+	else if (nbFrameInStateX > 27) {
+		nextFrame = 3;
+	}
+	else if (nbFrameInStateX > 14) {
+		nextFrame = 1;
+	}
+	else
+		nextFrame = 0;
+}
+
+void ShipAnimation::update(float deltaT, const Inputs& inputs) {
+	LinearAnimation::update(deltaT, inputs);
+}
+
+bool ShipAnimation::init(const ContentManager& contentManager)
+{
+	sf::Texture mainTexture = contentManager.getMainCharacterTexture();
+
+	InputBasedAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, hardLeft));
+	InputBasedAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, left));
+	// Si aucun mouvement horizontal
+	InputBasedAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, neutral1));
+	InputBasedAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, right));
+	InputBasedAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, hardRight));
+
+	LinearAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, neutral1));
+	LinearAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, neutral2));
+	LinearAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, neutral3));
+	LinearAnimation::frames.push_back(AnimationFrame::AnimationFrame(mainTexture, neutral4));
+
+	LinearAnimation::frames.begin();
+
+	return true;
+}
